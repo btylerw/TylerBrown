b0b07065d2a6ecf8438cae5449c8b7bd7da818e5
Tyler Brown <brown.tyler.w@gmail.com>
2024-01-11 13:20:17 -0800

    updated formsubmit url

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function submitForm() {
     // Sends e-mail without redirecting to formsubmit screen
     const form = document.querySelector('#contact-form');
     const data = new FormData(form);
-    const url = 'https://formsubmit.co/brown.tyler.w@gmail.com';
+    const url = 'https://formsubmit.co/d00c3190cb3a71d4c491498544cd3aea';
     fetch(url, {method: 'POSt', body: data});
     // Edit contact container to confirm message sent
     const contact = document.querySelector('#contact');
